721a8752ae3071c1908e94fab0068dd5343b6f1d
Weronika <szperkaweronika0@gmail.com>
2023-07-07 17:51:49 +0200

    changes in fetch logic

--- src/AdviceCard.js
+++ src/AdviceCard.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from "./AdviceCard.module.css";
 import DividerMobile from './images/PatternDividerMobile.svg';
 /*import DividerDesktop from './images/PatternDividerDesktop.svg';*/
 import IconDice from './images/icon-dice.svg';
 
-
+const url = 'https://api.adviceslip.com/advice';
+    const fetchAdvice = async () => {
+        const res = await fetch(url);
+        const slipObj = await res.json();
+        const newAdvice = {
+            id: slipObj.slip.id,
+            advice: slipObj.slip.advice
+        };
+        console.log(newAdvice)
+        return newAdvice;
+    };
 
 export default function AdviceCard() {
+
     const [data, setData] = React.useState(null);
-    const url = 'https://api.adviceslip.com/advice';
+    const [loading, setLoading] = React.useState(false);
+    //const [error, setError] = React.useState(null);
     
-    React.useEffect(() => {
-        
-        const fetchAdvice = async () => {
-            try{
-                const res = await fetch(url);
-                const newData = await res.json();
-                setData(newData.slip)
-                console.log(newData.slip)
-            } catch (error) {
-                console.log(error);
-            }
+    useEffect(() => {
+        let ignore = false;
+        const handleFetchData = async () => {
+            setLoading(true);
+            setData({});
+            const newData = await fetchAdvice();
+            if(ignore) return;
+            setData(newData);
+            setLoading(false);
+        };
+        handleFetchData();
+        return () => {
+            ignore=true;
         }
-        fetchAdvice();
+        
     }, [])
+
+    const handleClick = () => {
+        setLoading(true);
+    };
 
     return (
         <>
@@ -31,13 +49,13 @@ export default function AdviceCard() {
         <div className={styles.cardBack}>
             <div className={styles.advice}>
                 <h2>ADVICE {data.id}</h2>
-                <p>{data.advice}</p> 
+                <p>{loading ? "Please wait" : data.advice}</p> 
             </div>
             <div className={styles.divider}>
                 <img src={DividerMobile} alt='divider'/>
             </div>
             <div className={styles.btnContainer}>
-            <button className={styles.btnDice} >
+            <button className={styles.btnDice} onClick={handleClick}>
                 <img src={IconDice} alt='dice'/>
             </button>
             </div>
