5ffdb3327466b9dece05775d2512db41e3cc6d2d
Weronika <szperkaweronika0@gmail.com>
2023-07-08 14:18:12 +0200

    changes in logic

--- src/AdviceCard.js
+++ src/AdviceCard.js
@@ -1,55 +1,43 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from "./AdviceCard.module.css";
 import DividerMobile from './images/PatternDividerMobile.svg';
 /*import DividerDesktop from './images/PatternDividerDesktop.svg';*/
 import IconDice from './images/icon-dice.svg';
 
-const url = 'https://api.adviceslip.com/advice';
-    const fetchAdvice = async () => {
-        const res = await fetch(url);
-        const slipObj = await res.json();
-        const newAdvice = {
-            id: slipObj.slip.id,
-            advice: slipObj.slip.advice
-        };
-        console.log(newAdvice)
-        return newAdvice;
-    };
-
 export default function AdviceCard() {
 
-    const [data, setData] = React.useState(null);
-    const [loading, setLoading] = React.useState(false);
-    //const [error, setError] = React.useState(null);
-    
-    useEffect(() => {
-        let ignore = false;
-        const handleFetchData = async () => {
-            setLoading(true);
-            setData({});
-            const newData = await fetchAdvice();
-            if(ignore) return;
-            setData(newData);
-            setLoading(false);
-        };
-        handleFetchData();
-        return () => {
-            ignore=true;
-        }
-        
-    }, [])
-
-    const handleClick = () => {
-        setLoading(true);
+    const [data, setData] = React.useState("example");
+   
+    const url = 'https://api.adviceslip.com/advice';
+    const fetchAdvice = () => {
+        fetch(url)
+            .then(response => { return response.json() })
+            .then(newData  => {setData({ id: newData.slip.id, advice: newData.slip.advice }); })
+            
     };
 
+    React.useEffect(() => {
+        fetchAdvice();
+    }, [])
+
+    const handleClick = async () => {
+        try {
+            const fetchData = await fetch(url);
+            const newData = await fetchData.json();
+            console.log(newData)
+            setData({ id: newData.slip.id, advice: newData.slip.advice });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
     return (
         <>
         { data &&
         <div className={styles.cardBack}>
             <div className={styles.advice}>
-                <h2>ADVICE {data.id}</h2>
-                <p>{loading ? "Please wait" : data.advice}</p> 
+                <h2>ADVICE {data.id} </h2>
+                <p>{data.advice}</p> 
             </div>
             <div className={styles.divider}>
                 <img src={DividerMobile} alt='divider'/>
